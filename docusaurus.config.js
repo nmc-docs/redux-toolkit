376faf0585d4ac6a4cfb5fec58ec074c65107a64
Nguyễn Minh Chí <minhchico300kc@gmail.com>
2024-09-12 13:15:43 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
         },
         items: [
           {
-            href: "https://github.com/minhchi1509",
+            href: "https://github.com/nmc-docs/redux-toolkit",
             label: "GitHub",
             position: "right",
           },
